13fb2cf1ca791702119ab3ac9836d2f11ac67d45
Andrés Fernández <andresavena19@gmail.com>
2022-07-25 05:09:10 -0500

    feat: added change price log

--- subgraphs/harvest-finance/src/mappings/VaultListener.ts
+++ subgraphs/harvest-finance/src/mappings/VaultListener.ts
@@ -1,5 +1,7 @@
+import { decimal } from "@protofire/subgraph-toolkit";
 import { DoHardWorkCall } from "../../generated/ControllerListener/ControllerContract";
 import { Deposit, StrategyAnnounced, StrategyChanged } from "../../generated/ControllerListener/VaultContract";
+import { Withdraw } from "../../generated/schema";
 import { accounts, vaults } from "../modules";
 
 export function handleDeposit(event: Deposit): void {
@@ -8,10 +10,13 @@ export function handleDeposit(event: Deposit): void {
 	account.save()
 	// Increase protocl users
 
+
+
 	let vault = vaults.loadOrCreateVault(event.address)
 	vault.save()
 }
 
+export function handleWithdraw(event: Withdraw): void { }
 export function handleStrategyAnnounced(event: StrategyAnnounced): void { }
 export function handleStrategyChanged(event: StrategyChanged): void { }
 export function handleDoHardWorkCall(call: DoHardWorkCall): void { }
