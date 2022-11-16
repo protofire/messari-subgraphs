dccf21977e22c092baeee4724f453b47af2c8e2f
0xLucca <0xlucca.dev@gmail.com>
2022-11-16 16:25:13 -0300

    Track stakedOutputTokenAmount from NoMintRewardPools

--- subgraphs/harvest-finance/src/noMintRewardPool.ts
+++ subgraphs/harvest-finance/src/noMintRewardPool.ts
@@ -1,14 +1,14 @@
 import {
   Staked as StakedEvent,
   Withdrawn as WithdrawnEvent,
-} from '../generated/templates/PotPool/PotPool'
-import { getVaultFromPotPool } from './utils/potPools'
+} from '../generated/templates/NoMintRewardPool/NoMintRewardPool'
+import { getVaultFromNoMintRewardPool } from './utils/noMintRewardPools'
 
 export function handleStaked(event: StakedEvent): void {
   const amount = event.params.amount
   const poolAddress = event.address
 
-  const vault = getVaultFromPotPool(poolAddress)
+  const vault = getVaultFromNoMintRewardPool(poolAddress)
 
   if (!vault) return
 
@@ -21,7 +21,7 @@ export function handleWithdrawn(event: WithdrawnEvent): void {
   const amount = event.params.amount
   const poolAddress = event.address
 
-  const vault = getVaultFromPotPool(poolAddress)
+  const vault = getVaultFromNoMintRewardPool(poolAddress)
 
   if (!vault) return
 
