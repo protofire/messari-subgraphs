f315e9c7ebd21effc49ddb412f1bed627616fe95
0xLucca <0xlucca.dev@gmail.com>
2022-11-16 09:29:26 -0300

    Add describe with the handler name

--- subgraphs/harvest-finance/tests/megaFactory.test.ts
+++ subgraphs/harvest-finance/tests/megaFactory.test.ts
@@ -10,49 +10,51 @@ describe('MegaFactory', () => {
     clearStore()
   })
 
-  test('Create vault, tokens and rewardTokens', () => {
-    //Create vault
-    const vault = createVault()
+  describe('handleDeploymentCompleted', () => {
+    test('Create vault, tokens and rewardTokens', () => {
+      //Create vault
+      const vault = createVault()
 
-    //Create token
-    const tokenAddress = Address.fromString(
-      '0x0000000000000000000000000000000000000004'
-    )
-    helpers.mocking.erc20.erc20(tokenAddress, 'FARM', 'FARM', 18)
+      //Create token
+      const tokenAddress = Address.fromString(
+        '0x0000000000000000000000000000000000000004'
+      )
+      helpers.mocking.erc20.erc20(tokenAddress, 'FARM', 'FARM', 18)
 
-    //Create pool
-    const poolAddress = Address.fromString(
-      '0x0000000000000000000000000000000000000005'
-    )
-    helpers.mocking.potPool.potPool(poolAddress, tokenAddress)
+      //Create pool
+      const poolAddress = Address.fromString(
+        '0x0000000000000000000000000000000000000005'
+      )
+      helpers.mocking.potPool.potPool(poolAddress, tokenAddress)
 
-    //Create megaFactory
-    const megaFactoryAddress = Address.fromString(
-      '0xe1ec9151eb8d9a3451b8f623ce8b62632a6d4f4d'
-    )
-    helpers.mocking.megaFactory.megaFactory(
-      megaFactoryAddress,
-      tokenAddress,
-      poolAddress,
-      Address.fromString(vault.id),
-      '0'
-    )
+      //Create megaFactory
+      const megaFactoryAddress = Address.fromString(
+        '0xe1ec9151eb8d9a3451b8f623ce8b62632a6d4f4d'
+      )
+      helpers.mocking.megaFactory.megaFactory(
+        megaFactoryAddress,
+        tokenAddress,
+        poolAddress,
+        Address.fromString(vault.id),
+        '0'
+      )
 
-    //Create event
-    const event = helpers.createDeploymentCompletedEvent('0')
+      //Create event
+      const event = helpers.createDeploymentCompletedEvent('0')
 
-    //Handler
-    handleDeploymentCompleted(event)
+      //Handler
+      handleDeploymentCompleted(event)
 
-    //Assert reward token created
-    assert.assertNotNull(RewardToken.load(tokenAddress.toHexString()))
+      //Assert reward token created
+      assert.assertNotNull(RewardToken.load(tokenAddress.toHexString()))
 
-    //Assert vault has reward token
-    assert.fieldEquals(
-      'Vault',
-      vault.id,
-      'rewardTokens',
-      helpers.toStringArray([tokenAddress.toHexString()])
-    )
+      //Assert vault has reward token
+      assert.fieldEquals(
+        'Vault',
+        vault.id,
+        'rewardTokens',
+        helpers.toStringArray([tokenAddress.toHexString()])
+      )
+    })
   })
 })
